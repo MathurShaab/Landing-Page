202a5a2627a3396744b05b38cac8be750d5bc49e
MathurShaab <jatin.mathur0710@gmail.com>
2023-01-27 19:47:01 +0530

    first Commit

---
+++ assets/js/index.php
@@ -0,0 +1,276 @@
+<?php
+include"dbconn.php";
+$navlink='home';
+
+ ?>
+<!doctype html>
+<html lang="en-US">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="keywords" content="">
+        <!-- Links of CSS files -->
+        <link rel="stylesheet" href="assets/css/bootstrap..min.css">
+        <link rel="stylesheet" href="assets/css/boxicons.min.css">
+        <link rel="stylesheet" href="assets/css/flaticon.css">
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/css/odometer.min.css">
+        <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+        <link rel="stylesheet" href="assets/css/animate.min.css">
+        <link rel="stylesheet" href="assets/css/nice-select.min.css">
+        <link rel="stylesheet" href="assets/css/viewer.min.css">
+        <link rel="stylesheet" href="assets/css/slick.min.css">
+        <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/responsive.css">
+
+        <title>ASHA-Homecaretaker</title>
+
+        <link rel="icon" type="image/png" href="assets/img/favicon.png">
+		
+    </head>
+
+    <body>
+
+        <!-- Preloader -->
+        <div class="preloader">
+            <div class="loader">
+                <div class="shadow"></div>
+                <div class="box"></div>
+            </div>
+        </div>
+        <!-- End Preloader -->
+
+        <!-- Start Header Area -->
+        <?php include "header.php"; ?>
+        <!-- End Header Area -->
+
+      
+
+        <!-- Start Main Banner -->
+        <section class="home-slides owl-carousel owl-theme">
+            <div class="main-banner item-bg1">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+			<div class="main-banner item-bg2">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+			
+			<div class="main-banner item-bg4">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+           
+			 
+           
+        </section>
+        <!-- End Main Banner -->
+		<!-- Start Main Banner -->
+        <section class="banner-wrapper">
+            <div class="container">
+                <div class="row align-items-center">
+
+                    <div class="col-lg-12 col-md-12">
+                        <div class="banner-courses-category">
+                            <ul>
+                               
+                              <li>
+                                    <a href="https://wa.me/+918368607245?text=Hi" target="_blank">
+                                       <img src="assets/img/icons/driver.png" /><br>
+                                        Driver
+                                    </a>
+                                </li>
+
+                                
+
+                                <li>
+                                    <a href="https://wa.me/+918368607245?text=Hi" target="_blank">
+                                        <img src="assets/img/icons/cleaner.png" /><br>
+                                        Cleaning
+                                    </a>
+                                </li>
+
+                              
+
+                                <li>
+                                    <a href="https://wa.me/+918368607245?text=Hi" target="_blank">
+                                           <img src="assets/img/icons/helper.png" /><br>
+                                        Helper
+                                    </a>
+                                </li>
+
+                              
+
+                              
+
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Main Banner -->
+		<!-- Start About Area -->
+        <section class="about-area ptb-100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="about-image">
+                            <img src="assets/img/ab1.png" class="shadow" alt="image">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-12">
+                        <div class="about-content">
+                           
+                            <h2 style="color:white">Welcome to ASHA Home Care Takers family!</h2>
+                           
+                            <p style="color:white;text-align:justify">Think about Quick services, Think Asha Home Care Takers. <br>
+ASHA Home Care Takers Private Limited (ASHA HCT) is a one stop place for all services in relation to Standby Help/ Maid, Instant Driver in Delhi NCR. When grocery and food can be delivered in less than 60 minutes, how about getting a Stand-by Help / Driver in less than 60 minutes. We call them HOME CARE TAKERS (“HCT”).  
+</p>
+                           <h3 style="color:white">Vision</h3>
+                                <p style="color:white;text-align:justify">One stop place to find reliable, trained and professional Home Care Takers (Stand by-helpers, Drivers, Cooks, Car cleaning etc.). We endeavour to be 1 stop shop for all part time Home Care Takers.</p>
+							 <a href="about-us" class="default-btn"><i class='bx bx-user-pin icon-arrow before'></i><span class="label">Read More</span><i class="bx bx-user-pin icon-arrow after"></i></a>
+                           
+                        </div>
+                               
+                    </div>
+                </div>
+			</div>
+		</section>	
+		<!-- Start Experience Area -->
+        <section class="experience-area pt-100 bg-f5faf8">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-12 col-md-12">
+                        <div class="experience-content">
+                            <span class="sub-title">Why Asha Home Care Takers:</span>
+                           
+                            <ul class="features-list">
+                                <li><span><i class="bx bx-check"></i> Trusted, professional & reliable Home Care Takers</span></li>
+                                <li><span><i class="bx bx-check"></i> One point of contact with 24*7 Chat BoT support and Customer care </span></li>
+                                <li><span><i class="bx bx-check"></i> Private Limited Company founded by Lawyers and CAs with more than 20 years of industry experience</span></li>
+                                <li><span><i class="bx bx-check"></i> Served more than 300 plus clients & 1500 plus requests in Delhi NCR</span></li>
+                                <li><span><i class="bx bx-check"></i> More than 90% clients with 4 / 5-star ratings</span></li>
+                            </ul>
+                            <a href="#" class="default-btn">PS: We do not provide permanent helps / maids/ cooks, elderly patient care. </a>
+                        </div>
+                    </div>
+
+                  
+                </div>
+
+
+        </section>
+		 <section class="experience-area ptb-100 bg-f5faf8">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-12 col-md-12">
+                        <div class="experience-content">
+                            <span class="sub-title">What we do</span>
+                           
+                            <ul class="features-list">
+                                <li><span><i class="bx bx-check"></i>1.	Stand by Maid / Help: We provide stand-by help/ Maid for all purpose home services in flat 60 minutes in Delhi NCR. All Asha HCT come with no frills and are fully trained for all chores after undergoing professional training. Just say hi on our Whats App Chat BOT to know more.</li>
+
+ <li><span><i class="bx bx-check"></i>2.	Instant Driver: Need a driver for few hours or for few days? Look no further. We provide reliable & trustworthy drivers after thorough training and verification in flat 60 minutes in Delhi NCR. Just say hi on our Whats App Chat BOT to know more.</li>
+
+ <li><span><i class="bx bx-check"></i>3.	Permanent Driver: Problem finding reliable drivers, managing leaves, payments. Look no further, we provide Permanent driver after thorough training for soft skills and certification from Maruti Driving School. Besides, each driver is provided with company dress for summers and winters separately. Just say hi on our Whats App Chat BOT to know more.</li>
+
+ <li><span><i class="bx bx-check"></i>4.	Daily Home cleaning / office cleaning: Tired of helpers taking leaves without notice, home not cleaned like your corporate office. Look no further, we provide Daily 5-star cleaning for your homes and office with professionals training and tracking of work maintained by Asha HCT. Guess what, we provide back-up of helper when is on leave so that your home is never without a help 365 days.</li>
+                            </ul>
+                         
+                        </div>
+                    </div>
+
+                  
+                </div>
+
+
+        </section>
+        <!-- End Experience Area -->
+		<!-- Start Funfacts Area -->
+        <section class="funfacts-area">
+            <div class="container">
+                <div class="funfacts-inner">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-12">
+                          <h5>Our GOAL is to provide an unmatched experience to every Client / User, we want to grow but only with Happy Customers.</h5>
+                        </div>
+
+                        
+                    </div>
+
+                    <div id="particles-js-circle-bubble"></div>
+                </div>
+            </div>
+        </section>
+        <!-- End Funfacts Area -->
+		 <!-- Start Become Instructor & Partner Area -->
+        <section class="become-instructor-partner-area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6">
+                        <div class="become-instructor-partner-content bg-color">
+                            <h2>Become Partner with us</h2>
+                            <p>In case you are keen to join ASHA HCT as City Champion / Area Champion, ASHA HCT has some interesting opportunities to partner with you. Drop us an email </p>
+                            <a href="tel:+918130820004" class="default-btn" target="_blank"><i class='bx bx-phone-call icon-arrow before'></i><span class="label">Call Us</span><i class="bx bx-phone-call icon-arrow after"></i></a>  
+							<a href="https://wa.me/+918130820004?text=Hi" class="default-btn" target="_blank"><i class='bx bxl-whatsapp icon-arrow before'></i><span class="label">Whatsapp</span><i class="bx bxl-whatsapp icon-arrow after"></i></a>
+							  <a href="mailto:piyushchopra@gmail.com" class="default-btn" target="_blank"><i class='bx bx-envelope icon-arrow before'></i><span class="label">Email Us</span><i class="bx bx-envelope icon-arrow after"></i></a>
+                        </div>
+                    </div>
+					 <div class="col-lg-6 col-md-6">
+                        <div class="become-instructor-partner-image bg-image2 jarallax" data-jarallax='{"speed": 0.3}'>
+                            <img src="assets/img/become-partner.jpg" alt="image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Become Instructor & Partner Area -->
+		
+
+<?php include "footer.php"; ?>
+      
+
+        
+        <div class="go-top"><i class='bx bx-up-arrow-alt'></i></div>
+
+        <!-- Links of JS files -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <script src="assets/js/mixitup.min.js"></script>
+        <script src="assets/js/parallax.min.js"></script>
+        <script src="assets/js/jquery.appear.min.js"></script>
+        <script src="assets/js/odometer.min.js"></script>
+        <script src="assets/js/particles.min.js"></script>
+        <script src="assets/js/meanmenu.min.js"></script>
+        <script src="assets/js/jquery.nice-select.min.js"></script>
+        <script src="assets/js/viewer.min.js"></script>
+        <script src="assets/js/slick.min.js"></script>
+        <script src="assets/js/jquery.magnific-popup.min.js"></script>
+        <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
